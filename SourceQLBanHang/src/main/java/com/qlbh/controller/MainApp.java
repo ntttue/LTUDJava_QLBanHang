aeb7fc5723fee6ea6aea7044adcd49dc3f9867e1
ntttue <ntttue.gl@gmail.com>
2016-09-08 22:06:16 +0700

    [ADD] man hinh them nhan vien

--- SourceQLBanHang/src/main/java/com/qlbh/controller/MainApp.java
+++ SourceQLBanHang/src/main/java/com/qlbh/controller/MainApp.java
@@ -15,9 +15,9 @@ public class MainApp extends Application {
 	public void start(Stage primaryStage) {
 		Parent root;
 		try {
-			root = FXMLLoader.load(getClass().getResource("../fxml/chucnang/ChungTu.fxml"));
+			root = FXMLLoader.load(getClass().getResource("../fxml/danhmuc/ThemNhanVien.fxml"));
 			Scene scene = new Scene(root);
-			primaryStage.setTitle("Chứng từ");
+			primaryStage.setTitle("Thêm nhân viên");
 			primaryStage.initStyle(StageStyle.UTILITY);
 			primaryStage.setScene(scene);
 			primaryStage.show();
