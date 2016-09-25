7d51098f417874fe993cb329e5456c3611fc4cc3
ntttue <ntttue.gl@gmail.com>
2016-09-25 13:33:00 +0700

    update

--- SourceQLBanHang/src/main/java/com/qlbh/util/HibernateUtil.java
+++ SourceQLBanHang/src/main/java/com/qlbh/util/HibernateUtil.java
@@ -5,33 +5,84 @@
  */
 package com.qlbh.util;
 
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
+@SuppressWarnings("deprecation")
 public class HibernateUtil {
+	private static SessionFactory factory = null;
+	private static Configuration conf;
 
-	private static final SessionFactory sessionFactory;
-
-	static {
+	public static SessionFactory buildSessionFactory() {
 		try {
-			// Create the SessionFactory from hibernate.cfg.xml
-			Configuration configuration = new Configuration();
-			configuration.configure("hibernate.cfg.xml");
-			StandardServiceRegistryBuilder ssrb = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
-			sessionFactory = configuration.buildSessionFactory(ssrb.build());
-			Session session = sessionFactory.openSession();
-			
+			conf = new Configuration();
+			conf.configure("hibernate.cfg.xml");
+
+			System.out.println("configuration");
+			factory = conf.buildSessionFactory();
+			return factory;
 		} catch (Throwable ex) {
-			// Make sure you log the exception, as it might be swallowed
 			System.err.println("Initial SessionFactory creation failed." + ex);
 			throw new ExceptionInInitializerError(ex);
 		}
 	}
 
 	public static SessionFactory getSessionFactory() {
-		return sessionFactory;
+		if (factory == null) {
+			factory = buildSessionFactory();
+		}
+		return factory;
 	}
 
 }
+// private static SessionFactory sessionFactory;
+// private static ServiceRegistry serviceRegistry;
+//
+// static {
+// try {
+// Configuration configuration = new Configuration().configure();
+// serviceRegistry = new
+// ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+// sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+// } catch (Throwable ex) {
+// System.err.println("Initial SessionFactory creation failed." + ex);
+// throw new ExceptionInInitializerError(ex);
+// }
+// }
+//
+// public static SessionFactory getSessionFactory() {
+// return sessionFactory;
+// }
+
+// }
+// import org.hibernate.Session;
+// import org.hibernate.SessionFactory;
+// import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+// import org.hibernate.cfg.Configuration;
+//
+// public class HibernateUtil {
+//
+// private static final SessionFactory sessionFactory;
+//
+// static {
+// try {
+// // Create the SessionFactory from hibernate.cfg.xml
+// Configuration configuration = new Configuration();
+// configuration.configure("hibernate.cfg.xml");
+// StandardServiceRegistryBuilder ssrb = new
+// StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+// sessionFactory = configuration.buildSessionFactory(ssrb.build());
+// Session session = sessionFactory.openSession();
+//
+// } catch (Throwable ex) {
+// // Make sure you log the exception, as it might be swallowed
+// System.err.println("Initial SessionFactory creation failed." + ex);
+// throw new ExceptionInInitializerError(ex);
+// }
+// }
+//
+// public static SessionFactory getSessionFactory() {
+// return sessionFactory;
+// }
+//
+// }
