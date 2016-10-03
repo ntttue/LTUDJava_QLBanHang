28a5d08407f8dfd05e3bdebac5d0fd16ae187dc6
ntnahn <ntnahn@gmail.com>
2016-10-03 12:48:39 +0700

    Load Giao dien mua hang khi click Chucnang/MuaHang

--- SourceQLBanHang/src/main/java/com/qlbh/app/MainApp.java
+++ SourceQLBanHang/src/main/java/com/qlbh/app/MainApp.java
@@ -10,9 +10,18 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class MainApp extends Application {
+	private static Stage primaryStage; // **Declare static Stage**
 
+    private void setPrimaryStage(Stage stage) {
+    	MainApp.primaryStage = stage;
+    }
+
+    static public Stage getPrimaryStage() {
+        return MainApp.primaryStage;
+    }
 	@Override
 	public void start(Stage primaryStage) {
+		setPrimaryStage(primaryStage); // **Set the Stage**
 		Parent root;
 		try {
 			root = FXMLLoader.load(getClass().getResource("../fxml/ManHinhChinh.fxml"));
@@ -24,8 +33,6 @@ public class MainApp extends Application {
 			// Set application icon
 			primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("../images/ic_store_white_24dp.png")));
 			primaryStage.show();
-			
-			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
---
+++ SourceQLBanHang/src/main/java/com/qlbh/controller/ManHinhChinhController.java
@@ -0,0 +1,35 @@
+package com.qlbh.controller;
+import java.io.IOException;
+
+import com.qlbh.app.MainApp;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class ManHinhChinhController {
+	@FXML
+	private TabPane tabMainContent;
+	@FXML
+	private AnchorPane anchorPaneMainApp;
+	@FXML
+	void btnMuaHangClick(ActionEvent event) throws IOException {
+		System.out.println("Clicked on button 'Mua Hàng'!");       
+        Tab tab = new Tab();
+        tab.setText("Nhập hàng");
+        Parent root = (Parent) FXMLLoader.load(getClass().getResource("../fxml/chucnang/NhapHang.fxml"));
+        tab.setContent(root);
+        tabMainContent.getTabs().add(tab);
+        tabMainContent.getSelectionModel().select(tab);
+	}
+	@FXML
+	void btnKetThucClick(ActionEvent event) {
+		Stage primaryStage = MainApp.getPrimaryStage();
+		primaryStage.close();
+	}
+}
