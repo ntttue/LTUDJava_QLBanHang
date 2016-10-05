337bb1bd0c04f2940a4fce33785fa3a24595a1d1
ntttue <ntttue.gl@gmail.com>
2016-10-05 18:19:58 +0700

    update DangNhap

--- SourceQLBanHang/src/main/java/com/qlbh/controller/DangNhapController.java
+++ SourceQLBanHang/src/main/java/com/qlbh/controller/DangNhapController.java
@@ -1,31 +1,46 @@
 package com.qlbh.controller;
 
 import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXDialog;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import com.qlbh.app.MainApp;
 import com.qlbh.model.NguoidungHome;
 import com.qlbh.pojo.Nguoidung;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 
 public class DangNhapController {
-
+	@FXML
+	private AnchorPane mainAnchor;
 	@FXML
 	private JFXButton btnDangNhap;
 	@FXML
 	private JFXTextField txtTenDangNhap;
-
 	@FXML
 	private JFXPasswordField txtMatKhau;
 	@FXML
 	private StackPane stackPane;
+	@FXML
+	private Label lblError;
 
 	@FXML
 	void btnDangNhapClick(ActionEvent event) {
+		this.checkLogin();
+	}
+
+	@FXML
+	void onEnter(KeyEvent e) {
+		if (e.getCode().toString().equals("ENTER")) {
+			this.checkLogin();
+		}
+	}
+
+	private void checkLogin() {
 		String tenDangNhap = this.txtTenDangNhap.getText();
 		String matKhau = this.txtMatKhau.getText();
 		Nguoidung nd = new Nguoidung();
@@ -35,8 +50,7 @@ public class DangNhapController {
 		if (nd != null) {
 			MainApp.setUserLogin(nd);
 		} else {
-			
+			this.lblError.setText("Tên đăng nhập hoặc mật khẩu không đúng.");
 		}
-
 	}
 }
