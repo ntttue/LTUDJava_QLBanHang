66deabd016c3279cea687f61f74cb4a0083f42c8
TraiBlueSky <tran.van.trai.it1994@gmail.com>
2016-10-05 23:45:20 +0700

    nhap hang

--- SourceQLBanHang/src/main/java/com/qlbh/controller/chucnang/NhapHangController.java
+++ SourceQLBanHang/src/main/java/com/qlbh/controller/chucnang/NhapHangController.java
@@ -6,6 +6,8 @@ import java.util.List;
 import com.qlbh.model.KhohangHome;
 import com.qlbh.model.NhacungcapHome;
 import com.qlbh.model.NhanvienHome;
+import com.qlbh.pojo.Chitietphieunhap;
+import com.qlbh.pojo.Hanghoa;
 import com.qlbh.pojo.Khohang;
 import com.qlbh.pojo.Nhacungcap;
 import com.qlbh.pojo.Nhanvien;
@@ -21,9 +23,11 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.util.Callback;
 
 public class NhapHangController {
@@ -51,7 +55,8 @@ public class NhapHangController {
     @FXML
     private Button btnTaoMoi;
     @FXML
-    private TableView<Phieunhap> tablePhieuNhap;
+    private TableView<Chitietphieunhap> tableChiTiet;
+    private ObservableList<Chitietphieunhap> modelTableChiTiet;
     @FXML
     private TextArea txtGhiChu;
     @FXML
@@ -70,6 +75,7 @@ public class NhapHangController {
 	@FXML
     public void initialize() {
 		loadData();
+		initTableView();
     }
 	
 	
@@ -98,9 +104,9 @@ public class NhapHangController {
 			}
 		});
 		cmbMaNCC.setPromptText("Chọn mã nhà cung cấp");
-		//Load ten nha cc
+		//Load ten nha cung cap
 		cmbModelTenNCC = FXCollections.observableArrayList(nhacungcaps);
-		cmbTenNCC.setItems(cmbModelMaNhaCC);
+		cmbTenNCC.setItems(cmbModelTenNCC);
 		cmbTenNCC.setButtonCell(new TenNhaCungCapListCell());
 		cmbTenNCC.setCellFactory(new Callback<ListView<Nhacungcap>, ListCell<Nhacungcap>>() {
 			
@@ -109,6 +115,49 @@ public class NhapHangController {
 			}
 		});
 		cmbTenNCC.setPromptText("Chọn tên nhà cung cấp");
+	}
+	
+	private void initTableView(){
+		//STT
+		TableColumn colSTT = new TableColumn<>("STT");
+		//Ma hang
+		TableColumn<Chitietphieunhap, Hanghoa> colMaHang = new TableColumn<>("Mã hàng");
+		colMaHang.setCellValueFactory(new PropertyValueFactory<>(Chitietphieunhap.MA_HANG_HOA));
+		//Ten hang
+		TableColumn<Chitietphieunhap, Hanghoa> colTenHang = new TableColumn<>("Tên hàng");
+		colTenHang.setCellValueFactory(new PropertyValueFactory<>(Chitietphieunhap.TEN_HANG_HOA));
+		//Don vi
+		TableColumn<Chitietphieunhap, String> colDonVi = new TableColumn<>("Đơn vị");
+		colDonVi.setCellValueFactory(new PropertyValueFactory<>(Chitietphieunhap.DON_VI));
+		//So luong
+		TableColumn<Chitietphieunhap, Integer> colSoLuong = new TableColumn<>("Số lượng");
+		colSoLuong.setCellValueFactory(new PropertyValueFactory<>(Chitietphieunhap.SO_LUONG));
+		//Don gia
+		TableColumn<Chitietphieunhap, Double> colDonGia = new TableColumn<>("Đơn giá");
+		colDonGia.setCellValueFactory(new PropertyValueFactory<>(Chitietphieunhap.DON_GIA));
+		//Thanh tien
+		TableColumn<Chitietphieunhap, Double> colThanhTien = new TableColumn<>("Thành tiền");
+		colThanhTien.setCellValueFactory(new PropertyValueFactory<>(Chitietphieunhap.THANH_TIEN));
+		//Ghi chu
+		TableColumn<Chitietphieunhap, String> colGhiChu = new TableColumn<>("Ghi chú");
+		colGhiChu.setCellValueFactory(new PropertyValueFactory<>(Chitietphieunhap.GHI_CHU));
+		//Add column
+		tableChiTiet.getColumns().addAll(colMaHang, colTenHang, colSoLuong, colDonGia, colThanhTien, colGhiChu);
+		//set data
+		Hanghoa hanghoa = new Hanghoa();
+		hanghoa.setMa("MH00001");
+		hanghoa.setTen("Máy đông lạnh");
+		Hanghoa hanghoa2 = new Hanghoa();
+		hanghoa2.setMa("MH00010");
+		hanghoa2.setTen("Máy khoan");
+		Hanghoa hanghoa3 = new Hanghoa();
+		hanghoa3.setMa("MH00020");
+		hanghoa3.setTen("Máy sấy");
+		Chitietphieunhap chitietphieunhap = new Chitietphieunhap(hanghoa, new Phieunhap(), 5, 20000000d, 100000000d, "", true, "002");
+		Chitietphieunhap chitietphieunhap2 = new Chitietphieunhap(hanghoa2, new Phieunhap(), 3, 10000000d, 30000000d, "", true, "003");
+		Chitietphieunhap chitietphieunhap3 = new Chitietphieunhap(hanghoa3, new Phieunhap(), 1, 20000000d, 20000000d, "", true, "004");
+		modelTableChiTiet = FXCollections.observableArrayList(chitietphieunhap, chitietphieunhap2, chitietphieunhap3);
+		tableChiTiet.setItems(modelTableChiTiet);
 	}
 
 }
