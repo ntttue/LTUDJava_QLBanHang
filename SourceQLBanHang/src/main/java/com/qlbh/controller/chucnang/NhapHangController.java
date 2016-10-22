243f96f7a086a85be937e8bb6104b921c08cde3d
TraiBlueSky <tran.van.trai.it1994@gmail.com>
2016-10-22 14:11:59 +0700

    fix nhap hang

--- SourceQLBanHang/src/main/java/com/qlbh/controller/chucnang/NhapHangController.java
+++ SourceQLBanHang/src/main/java/com/qlbh/controller/chucnang/NhapHangController.java
@@ -17,10 +17,12 @@ import com.qlbh.render.combobox.TenNhaCungCapListCell;
 
 import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.adapter.JavaBeanObjectProperty;
 import javafx.beans.property.adapter.JavaBeanObjectPropertyBuilder;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
@@ -30,11 +32,13 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableColumn.CellDataFeatures;
+import javafx.scene.control.TableColumn.CellEditEvent;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.ComboBoxTableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.util.Callback;
 import javafx.util.StringConverter;
 
@@ -131,10 +135,10 @@ public class NhapHangController {
 		hanghoa.setMa("MH00001");
 		hanghoa.setTen("Máy đông lạnh");
 		Hanghoa hanghoa2 = new Hanghoa();
-		hanghoa2.setMa("MH00010");
+		hanghoa2.setMa("MH00002");
 		hanghoa2.setTen("Máy khoan");
 		Hanghoa hanghoa3 = new Hanghoa();
-		hanghoa3.setMa("MH00020");
+		hanghoa3.setMa("MH00003");
 		hanghoa3.setTen("Máy sấy");
 		ObservableList<Hanghoa> hanghoaList = FXCollections.observableArrayList(hanghoa, hanghoa2, hanghoa3);
 		Chitietphieunhap chitietphieunhap = new Chitietphieunhap(hanghoa, new Phieunhap(), 1, 1000000d, 1000000d, "Ghi chú 1", true, "002");
@@ -146,18 +150,15 @@ public class NhapHangController {
 		colSTT.setCellValueFactory(column-> new ReadOnlyObjectWrapper<Number>(tableChiTiet.getItems().indexOf(column.getValue()) + 1));
 		//Ma hang
 		TableColumn<Chitietphieunhap, Hanghoa> colMaHang = new TableColumn<>("Mã hàng");
-//		colMaHang.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getMa()));
-		final StringConverter<Hanghoa> converter = new StringConverter<Hanghoa>() {
+		final StringConverter<Hanghoa> converterMa = new StringConverter<Hanghoa>() {
 			
 			@Override
 			public String toString(Hanghoa object) {
-				// TODO Auto-generated method stub
 				return object.getMa();
 			}
 			
 			@Override
 			public Hanghoa fromString(String string) {
-				// TODO Auto-generated method stub
 				return null;
 			}
 		};
@@ -166,61 +167,109 @@ public class NhapHangController {
 
 			@Override
 			public ObservableValue<Hanghoa> call(CellDataFeatures<Chitietphieunhap, Hanghoa> param) {
-				// TODO Auto-generated method stub
 				try {
 					JavaBeanObjectPropertyBuilder<Hanghoa> builder = new JavaBeanObjectPropertyBuilder<>();
 					builder.bean(param.getValue());
-					builder.name("HangHoa");
+					builder.name("Hanghoa");
 					return (ObservableValue<Hanghoa>)builder.build();
 				} catch (NoSuchMethodException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 				return null;
 			}
 		});
 		
-		colMaHang.setCellFactory(new Callback<TableColumn<Chitietphieunhap,Hanghoa>, TableCell<Chitietphieunhap,Hanghoa>>() {
-
+		colMaHang.setCellFactory(ComboBoxTableCell.forTableColumn(converterMa, hanghoaList));
+		
+		colMaHang.setOnEditCommit( new EventHandler<TableColumn.CellEditEvent<Chitietphieunhap,Hanghoa>>() {
+			
 			@Override
-			public TableCell<Chitietphieunhap, Hanghoa> call(TableColumn<Chitietphieunhap, Hanghoa> param) {
-				// TODO Auto-generated method stub
-				return new ComboBoxTableCell<Chitietphieunhap, Hanghoa>(converter, hanghoaList){
-
-					@Override
-					public void updateItem(Hanghoa hanghoa, boolean arg1) {
-						// TODO Auto-generated method stub
-						super.updateItem(hanghoa, arg1);
-						if(hanghoa != null){
-							setText(hanghoa.getMa());
-						}
-					}
-					
-				};
+			public void handle(CellEditEvent<Chitietphieunhap, Hanghoa> event) {
+				event.getTableView().getItems().get(event.getTablePosition().getRow()).setHanghoa(event.getNewValue());
 			}
 		});
 		
 		//Ten hang
-		TableColumn<Chitietphieunhap, String> colTenHang = new TableColumn<>("Tên hàng");
-		colTenHang.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getHanghoa().getTen()));
+		TableColumn<Chitietphieunhap, Hanghoa> colTenHang = new TableColumn<>("Tên hàng");
+		colTenHang.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Chitietphieunhap,Hanghoa>, ObservableValue<Hanghoa>>() {
+
+			@Override
+			public ObservableValue<Hanghoa> call(CellDataFeatures<Chitietphieunhap, Hanghoa> param) {
+				try {
+					JavaBeanObjectPropertyBuilder<Hanghoa> builder = new JavaBeanObjectPropertyBuilder<>();
+					builder.bean(param.getValue());
+					builder.name("Hanghoa");
+					return (ObservableValue<Hanghoa>)builder.build();
+				} catch (NoSuchMethodException e) {
+					e.printStackTrace();
+				}
+				return null;
+			}
+		});
+		
+		final StringConverter<Hanghoa> converterTen = new StringConverter<Hanghoa>() {
+
+			@Override
+			public Hanghoa fromString(String string) {
+				return null;
+			}
+
+			@Override
+			public String toString(Hanghoa hanghoa) {
+				return hanghoa.getTen();
+			}
+		};
+		colTenHang.setCellFactory(ComboBoxTableCell.forTableColumn(converterTen, hanghoaList));
+		colTenHang.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<Chitietphieunhap,Hanghoa>>() {
+
+			@Override
+			public void handle(CellEditEvent<Chitietphieunhap, Hanghoa> event) {
+				event.getTableView().getItems().get(event.getTablePosition().getRow()).setHanghoa(event.getNewValue());
+				colMaHang.editableProperty();
+			}
+		});
 		//Don vi
 		TableColumn<Chitietphieunhap, String> colDonVi = new TableColumn<>("Đơn vị");
-		colDonVi.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getHanghoa().getDonvi()));
+		colDonVi.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getHanghoa().getDonvitinh().getTen()));
 		//So luong
 		TableColumn<Chitietphieunhap, String> colSoLuong = new TableColumn<>("Số lượng");
 		colSoLuong.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getSoluong().toString()));
+		colSoLuong.setCellFactory(TextFieldTableCell.forTableColumn());
+		colSoLuong.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<Chitietphieunhap,String>>() {
+
+			@Override
+			public void handle(CellEditEvent<Chitietphieunhap, String> event) {
+				
+			}
+		});
 		//Don gia
 		TableColumn<Chitietphieunhap, String> colDonGia = new TableColumn<>("Đơn giá");
 		colDonGia.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getDongia().toString()));
+		colDonGia.setCellFactory(TextFieldTableCell.forTableColumn());
+		colDonGia.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<Chitietphieunhap,String>>() {
+
+			@Override
+			public void handle(CellEditEvent<Chitietphieunhap, String> event) {
+				
+			}
+		});
 		//Thanh tien
 		TableColumn<Chitietphieunhap, String> colThanhTien = new TableColumn<>("Thành tiền");
 		colThanhTien.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getThanhtien().toString()));
 		//Ghi chu
 		TableColumn<Chitietphieunhap, String> colGhiChu = new TableColumn<>("Ghi chú");
 		colGhiChu.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getGhichu()));
+		colGhiChu.setCellFactory(TextFieldTableCell.forTableColumn());
+		colGhiChu.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<Chitietphieunhap,String>>() {
+
+					@Override
+					public void handle(CellEditEvent<Chitietphieunhap, String> event) {
+						
+					}
+		});
 		//Add column
 		tableChiTiet.getColumns().addAll(colSTT, colMaHang, colTenHang, colSoLuong, colDonGia, colThanhTien, colGhiChu);
-
+		tableChiTiet.setEditable(true);
 		modelTableChiTiet = FXCollections.observableArrayList(chitietphieunhap, chitietphieunhap2, chitietphieunhap3);
 		tableChiTiet.setItems(modelTableChiTiet);
 	}
