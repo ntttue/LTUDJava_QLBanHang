70dd0ba37e7d548fc97bb01b0ec135d0be470a8f
ntttue <ntttue.gl@gmail.com>
2016-10-23 20:07:56 +0700

    [ADD] sua bo phan cong ty

--- SourceQLBanHang/src/main/java/com/qlbh/controller/danhmuc/SuaBoPhanCongTyController.java
+++ SourceQLBanHang/src/main/java/com/qlbh/controller/danhmuc/SuaBoPhanCongTyController.java
@@ -3,12 +3,11 @@ package com.qlbh.controller.danhmuc;
 import com.jfoenix.controls.JFXButton;
 import com.qlbh.controller.common.DialogController;
 import com.qlbh.model.BophanHome;
-import com.qlbh.model.NguoidungHome;
 import com.qlbh.model.TygiaHome;
 import com.qlbh.pojo.Bophan;
 import com.qlbh.pojo.Tygia;
+import com.qlbh.util.DataInputUtils;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -16,7 +15,7 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-public class ThemBoPhanCongTyController {
+public class SuaBoPhanCongTyController {
 	@FXML
 	private AnchorPane root;
 	@FXML
@@ -26,37 +25,12 @@ public class ThemBoPhanCongTyController {
 	@FXML
 	private Label lblError;
 	BophanHome boPhanHome = new BophanHome();
-
-	@FXML
-	public void onButtonLuuClick() {
-		this.ThemBoPhan();
-	}
-
-	private void ThemBoPhan() {
-		if (this.txtMa.getLength() == 0 || this.txtTen.getLength() == 0) {
-			this.lblError.setText("Vui lòng điền đủ thông tin trong các mục (*)");
-			return;
-		}
-		if (boPhanHome.findByMa(this.txtMa.getText()) != null) {
-			this.lblError.setText("Mã bộ phận đã tồn tại. Vui lòng nhập lại.");
-			return;
-		}
-		Bophan bophan = new Bophan();
-		bophan.setTen(this.txtTen.getText());
-		bophan.setGhichu(this.txtGhiChu.getText());
-		bophan.setMa(this.txtMa.getText());
-		bophan.setActivity(true);
-		if (boPhanHome.saveReturnObj(bophan)) {
-			DanhSachBoPhanController.boPhanController.onBoPhanAdded();
-		} else {
-			DialogController.show(root, null, "Thông báo", "Thêm bộ phận không thành công. Vui lòng thử lại.", false);
-		}
-	}
+	Bophan bophan = new Bophan();
 
 	@FXML
 	void onEnter(KeyEvent e) {
 		if (e.getCode().toString().equals("ENTER")) {
-			this.ThemBoPhan();
+			this.SuaBoPhan();
 		}
 		if (e.getCode().toString().equals("ESCAPE")) {
 			this.closeStage();
@@ -64,10 +38,37 @@ public class ThemBoPhanCongTyController {
 	}
 
 	@FXML
+	public void onButtonLuuClick() {
+		this.SuaBoPhan();
+	}
+
+	private void SuaBoPhan() {
+		if (this.txtMa.getLength() == 0 || this.txtTen.getLength() == 0) {
+			this.lblError.setText("Vui lòng điền đủ thông tin trong các mục (*)");
+			return;
+		}
+		bophan.setTen(this.txtTen.getText());
+		bophan.setGhichu(this.txtGhiChu.getText());
+		if (boPhanHome.update(bophan)) {
+			DanhSachBoPhanController.boPhanController.onBophanUpdated();
+		} else {
+			DialogController.show(root, null, "Thông báo", "Sửa bộ phận không thành công, vui lòng thử lại.", false);
+		}
+	}
+
+	public void setBophan(Bophan obj) {
+		this.bophan = obj;
+		this.txtMa.setText(bophan.getMa());
+		this.txtTen.setText(bophan.getTen());
+		this.txtGhiChu.setText(bophan.getGhichu());
+	}
+
+	@FXML
 	public void onButtonDongClick() {
 		this.closeStage();
 	}
-	private void closeStage(){
+
+	private void closeStage() {
 		Stage stage = (Stage) btnDong.getScene().getWindow();
 		stage.close();
 	}
