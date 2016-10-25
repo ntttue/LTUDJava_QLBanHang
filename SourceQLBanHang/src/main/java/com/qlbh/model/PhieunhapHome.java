6a978401e12b4c14560ede728befc7cc6923e3f0
ntnahn <ntnahn@gmail.com>
2016-10-25 23:52:52 +0700

    Load bảng Phieuxuat để làm chưc năng Thu tiền

--- SourceQLBanHang/src/main/java/com/qlbh/model/PhieunhapHome.java
+++ SourceQLBanHang/src/main/java/com/qlbh/model/PhieunhapHome.java
@@ -17,59 +17,7 @@ import com.qlbh.pojo.Phieunhap;
  * @author Hibernate Tools
  */
 @Stateless
-public class PhieunhapHome extends AbstractDao{
-
-	private static final Log log = LogFactory.getLog(PhieunhapHome.class);
-
-	@PersistenceContext
-	private EntityManager entityManager;
-
-	public void persist(Phieunhap transientInstance) {
-		log.debug("persisting Phieunhap instance");
-		try {
-			entityManager.persist(transientInstance);
-			log.debug("persist successful");
-		} catch (RuntimeException re) {
-			log.error("persist failed", re);
-			throw re;
-		}
-	}
-
-	public void remove(Phieunhap persistentInstance) {
-		log.debug("removing Phieunhap instance");
-		try {
-			entityManager.remove(persistentInstance);
-			log.debug("remove successful");
-		} catch (RuntimeException re) {
-			log.error("remove failed", re);
-			throw re;
-		}
-	}
-
-	public Phieunhap merge(Phieunhap detachedInstance) {
-		log.debug("merging Phieunhap instance");
-		try {
-			Phieunhap result = entityManager.merge(detachedInstance);
-			log.debug("merge successful");
-			return result;
-		} catch (RuntimeException re) {
-			log.error("merge failed", re);
-			throw re;
-		}
-	}
-
-	public Phieunhap findById(Integer id) {
-		log.debug("getting Phieunhap instance with id: " + id);
-		try {
-			Phieunhap instance = entityManager.find(Phieunhap.class, id);
-			log.debug("get successful");
-			return instance;
-		} catch (RuntimeException re) {
-			log.error("get failed", re);
-			throw re;
-		}
-	}
-	
+public class PhieunhapHome extends AbstractDao {
 	public Phieunhap save(Phieunhap phieunhap){
 		super.save(phieunhap);
 		return phieunhap;
--- SourceQLBanHang/src/main/java/com/qlbh/model/PhieuxuatHome.java
+++ SourceQLBanHang/src/main/java/com/qlbh/model/PhieuxuatHome.java
@@ -1,71 +1,65 @@
 package com.qlbh.model;
 // Generated 24/09/2016 3:27:00 PM by Hibernate Tools 5.2.0.Beta1
 
+import java.util.List;
+
 import javax.ejb.Stateless;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.Logger;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
 
+import com.qlbh.model.common.AbstractDao;
+import com.qlbh.pojo.Keeplogged;
 import com.qlbh.pojo.Phieuxuat;
+import com.qlbh.util.DataAccessLayerException;
+import com.qlbh.util.HibernateFactory;
 
 /**
  * Home object for domain model class Phieuxuat.
+ * 
  * @see com.qlbh.model.Phieuxuat
  * @author Hibernate Tools
  */
 @Stateless
-public class PhieuxuatHome {
+public class PhieuxuatHome extends AbstractDao {
+	private static final Logger logger = Logger.getLogger(PhieuxuatHome.class);
+	private Session session;
 
-	private static final Log log = LogFactory.getLog(PhieuxuatHome.class);
-
-	@PersistenceContext
-	private EntityManager entityManager;
-
-	public void persist(Phieuxuat transientInstance) {
-		log.debug("persisting Phieuxuat instance");
-		try {
-			entityManager.persist(transientInstance);
-			log.debug("persist successful");
-		} catch (RuntimeException re) {
-			log.error("persist failed", re);
-			throw re;
-		}
+	public List findAll() throws DataAccessLayerException {
+//		 System.out.println("find all");
+		 return super.findAll(Phieuxuat.class);
+//		try {
+//			session = HibernateFactory.getSessionFactory().openSession();
+//			// startOperation();
+//			String hql = "from "+Phieuxuat.class+" where activity = true";
+//			Query query = session.createQuery(hql);
+//			List<Phieuxuat> ds = query.list();
+//			return ds;
+//		} catch (HibernateException e) {
+//			handleException(e);
+//			logger.error("error in findFirst:  \n" + e.getMessage());
+//		} finally {
+//			HibernateFactory.close(session);
+//		}
+//		return null;
 	}
 
-	public void remove(Phieuxuat persistentInstance) {
-		log.debug("removing Phieuxuat instance");
-		try {
-			entityManager.remove(persistentInstance);
-			log.debug("remove successful");
-		} catch (RuntimeException re) {
-			log.error("remove failed", re);
-			throw re;
-		}
+	public void save(Phieuxuat phieuXuat) {
+		super.save(phieuXuat);
 	}
 
-	public Phieuxuat merge(Phieuxuat detachedInstance) {
-		log.debug("merging Phieuxuat instance");
-		try {
-			Phieuxuat result = entityManager.merge(detachedInstance);
-			log.debug("merge successful");
-			return result;
-		} catch (RuntimeException re) {
-			log.error("merge failed", re);
-			throw re;
-		}
+	public void update(Phieuxuat phieuXuat) {
+		super.update(phieuXuat);
 	}
 
-	public Phieuxuat findById(Integer id) {
-		log.debug("getting Phieuxuat instance with id: " + id);
-		try {
-			Phieuxuat instance = entityManager.find(Phieuxuat.class, id);
-			log.debug("get successful");
-			return instance;
-		} catch (RuntimeException re) {
-			log.error("get failed", re);
-			throw re;
-		}
+	public void deletePermanently(Phieuxuat phieuXuat) {
+		super.delete(phieuXuat);
+	}
+
+	public void delete(Phieuxuat phieuXuat) {
+		phieuXuat.setActivity(false);
+		super.update(phieuXuat);
 	}
 }
